df11197f51c277b3202d64b54ddaefa6c6039ee4
David Jensen <david.lgj@gmail.com>
2015-05-28 16:25:29 +0200

    Support for readonly

--- test/tests.js
+++ test/tests.js
@@ -58,6 +58,51 @@ describe('Schema form', function() {
 
       });
     });
+
+
+    it('should be disabled when readonly', function() {
+
+      //Mock pickadate
+      $.fn.pickadate = sinon.stub().returns({
+        set: sinon.stub(),
+        get: sinon.stub().returns('get')
+      });
+      $.fn.pickadate.defaults = {format: 'yyyy-mm-dd'};
+
+      inject(function($compile, $rootScope) {
+        var scope = $rootScope.$new();
+        scope.person = {partee: '2014-01-01'};
+
+        scope.schema = {
+          type: 'object',
+          properties: {
+            partee: {
+              title: 'Parteeeeee',
+              type: 'string',
+              format: 'date'
+            }
+          }
+        };
+
+        scope.form = [{
+          key: 'partee',
+          maxDate: new Date(),
+          minDate: '2014-02-13',
+          readonly: true
+        }];
+
+        var tmpl = angular
+                  .element('<form sf-schema="schema" sf-form="form" sf-model="person"></form>');
+
+        $compile(tmpl)(scope);
+        $rootScope.$apply();
+        tmpl.children().eq(0).children().eq(0).find('input[pick-a-date]').attr('disabled').should.ok;
+
+        $.fn.pickadate.should.have.beenCalled;
+
+      });
+    });
+
   });
 
 });
